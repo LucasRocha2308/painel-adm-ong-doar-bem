39df30d2a0a399abe3c81da988fdf70a7188a9a7
caiomanfredini <caiomanfredini.15@gmail.com>
2021-07-25 01:47:11 -0300

    add componente ContentCard (principal)

---
+++ src/components/ContentCard/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './ContentCard.scss'
+
+const ContentCard = () => {
+    return (
+        <div className="main-content-space">
+            <div className="main-content">
+            </div>
+        </div>
+    )
+}
+
+export default ContentCard;
