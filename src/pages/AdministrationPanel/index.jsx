8df04919b7c8a5dcea322736130b710963e70f83
caiomanfredini <caiomanfredini.15@gmail.com>
2021-07-25 00:56:39 -0300

    update pagina administração / card principal v.1

--- src/pages/AdministrationPanel/index.jsx
+++ src/pages/AdministrationPanel/index.jsx
@@ -1,5 +1,7 @@
 import HeaderPanel from "../../components/HeaderPanel";
 import MenuLateral from "../../components/MenuLateral";
+import { AiOutlineDown, AiFillDelete, AiOutlineUserAdd } from 'react-icons/ai';
+
 import './styles.scss'
 
 const AdministrationPanel = () => {
@@ -7,6 +9,52 @@ const AdministrationPanel = () => {
     <div className="container">
       <HeaderPanel />
       <MenuLateral />
+      <div className="main-content-space">
+        <div className="main-content">
+          <h1>Olá mundo</h1>
+          <input type="text" name="search-user" id="search-user" placeholder="Pesquisar Usuário" />
+          <div className="users-table">
+            <table>
+              <tr>
+                <th>Nome<AiOutlineDown size={12} className="icon"/></th>
+                <th>Função<AiOutlineDown size={12} className="icon"/></th>
+                <th>Último Acesso<AiOutlineDown size={12} className="icon"/></th>
+                <th><AiOutlineUserAdd size={22} /></th>
+              </tr>
+              <tr>
+                <td>Jill</td>
+                <td>Smith</td>
+                <td>50</td>
+                <td><AiFillDelete size={20} /></td>
+              </tr>
+              <tr>
+                <td>Eve</td>
+                <td>Jackson</td>
+                <td>94</td>
+                <td><AiFillDelete size={20} /></td>
+              </tr>
+              <tr>
+                <td>Eve</td>
+                <td>Jackson</td>
+                <td>94</td>
+                <td><AiFillDelete size={20} /></td>
+              </tr>
+              <tr>
+                <td>Eve</td>
+                <td>Jackson</td>
+                <td>94</td>
+                <td><AiFillDelete size={20} /></td>
+              </tr>
+              <tr>
+                <td>Eve</td>
+                <td>Jackson</td>
+                <td>94</td>
+                <td><AiFillDelete size={20} /></td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
