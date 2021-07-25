138d72fd35e48a0445f454a558b09ea85e1ae0bc
caiomanfredini <caiomanfredini.15@gmail.com>
2021-07-25 20:51:30 -0300

    feat/Pagina SupportAdministration

--- src/pages/SupportAdministration/index.jsx
+++ src/pages/SupportAdministration/index.jsx
@@ -1,11 +1,7 @@
 import HeaderPanel from "../../components/HeaderPanel";
 import MenuLateral from "../../components/MenuLateral";
-
-// import ContentCard from "../../components/ContentCard";
-
+import ContentCard from "../../components/ContentCard";
 import FooterPanel from "../../components/FooterPanel";
-
-import { AiOutlineDown, AiFillDelete, AiOutlineUserAdd } from 'react-icons/ai';
 
 import './styles.scss'
 
@@ -14,56 +10,13 @@ const SupportAdministration = () => {
     <div className="container">
       <HeaderPanel />
       <MenuLateral />
-
-      {/* <ContentCard> */}
-      <div className="main-content-space">
-        <div className="main-content">
-          <h1>Olá mundo SUPORTE</h1>
-          <input type="text" name="search-user" id="search-user" placeholder="Pesquisar Usuário" />
-          <div className="users-table">
-            <table>
-              <tr>
-                <th>Nome<AiOutlineDown size={12} className="icon"/></th>
-                <th>Função<AiOutlineDown size={12} className="icon"/></th>
-                <th>Último Acesso<AiOutlineDown size={12} className="icon"/></th>
-                <th><AiOutlineUserAdd size={22} /></th>
-              </tr>
-              <tr>
-                <td>Jill</td>
-                <td>Smith</td>
-                <td>50</td>
-                <td><AiFillDelete size={20} /></td>
-              </tr>
-              <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
-                <td><AiFillDelete size={20} /></td>
-              </tr>
-              <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
-                <td><AiFillDelete size={20} /></td>
-              </tr>
-              <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
-                <td><AiFillDelete size={20} /></td>
-              </tr>
-              {/* <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
-                <td><AiFillDelete size={20} /></td>
-              </tr> */}
-            </table>
-          </div>
+      <ContentCard>
+        <div className="support-panel">
+          <p className="main-text">Para visualizar todos os contatos recebidos via ChatBot é preciso fazer o login na plataforma Landbot clicando no botão abaixo:</p>
+          <button className="chatbot-button">Acessar ChatBot</button>
         </div>
-      </div>
-      {/* </ContentCard> */}
-      <FooterPanel/>
+      </ContentCard>
+      <FooterPanel />
 
     </div>
   );
