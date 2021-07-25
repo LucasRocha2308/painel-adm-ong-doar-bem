a952665d36f2de87e88f1604b26c2c5145cadc8d
rafael cruz <lucas_rocha_25@hotmail.com>
2021-07-25 20:09:11 -0300

    feat/criação da pagina DonorAdministration

--- src/pages/DonorAdministration/index.jsx
+++ src/pages/DonorAdministration/index.jsx
@@ -1,10 +1,7 @@
 import HeaderPanel from "../../components/HeaderPanel";
 import MenuLateral from "../../components/MenuLateral";
-
 // import ContentCard from "../../components/ContentCard";
-
 import FooterPanel from "../../components/FooterPanel";
-
 import { AiOutlineDown, AiFillDelete, AiOutlineUserAdd } from 'react-icons/ai';
 
 import './styles.scss'
@@ -24,32 +21,32 @@ const DonorAdministration = () => {
             <table>
               <tr>
                 <th>Nome<AiOutlineDown size={12} className="icon"/></th>
-                <th>Função<AiOutlineDown size={12} className="icon"/></th>
-                <th>Último Acesso<AiOutlineDown size={12} className="icon"/></th>
+                <th>Campanha<AiOutlineDown size={12} className="icon"/></th>
+                <th>Última doação<AiOutlineDown size={12} className="icon"/></th>
                 <th><AiOutlineUserAdd size={22} /></th>
               </tr>
               <tr>
-                <td>Jill</td>
-                <td>Smith</td>
-                <td>50</td>
+                <td>Alexandre Souza</td>
+                <td>Campanha Sol</td>
+                <td>01/01/2001 às 15h32</td>
                 <td><AiFillDelete size={20} /></td>
               </tr>
               <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
+                <td>Bruno Souza</td>
+                <td>Campanha de Sucesso</td>
+                <td>09/05/2021 às 23h40</td>
                 <td><AiFillDelete size={20} /></td>
               </tr>
               <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
+                <td>Alexandre Souza</td>
+                <td>Campanha Sol</td>
+                <td>09/05/2021 às 23h40</td>
                 <td><AiFillDelete size={20} /></td>
               </tr>
               <tr>
-                <td>Eve</td>
-                <td>Jackson</td>
-                <td>94</td>
+                <td>Bruno Souza</td>
+                <td>Campanha de Sucesso</td>
+                <td>09/05/2021 às 23h40</td>
                 <td><AiFillDelete size={20} /></td>
               </tr>
               {/* <tr>
